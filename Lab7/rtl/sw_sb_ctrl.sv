eb3d6dae0ddee064ae8607c2917ee6d9a6bb3bd2
Fialka-Labs <ruslan@ruslan-pc.localdomain>
2026-06-16 22:34:49 +0300

    new_commit

--- Lab7/rtl/sw_sb_ctrl.sv
+++ Lab7/rtl/sw_sb_ctrl.sv
@@ -1,40 +1,48 @@
 `timescale 1ns / 1ps;
 
 module sw_sb_ctrl(
-    // Интерфейс системной шины, предназначенный для взаимодействия с внешним миром
+    // Часть интерфейса модуля, отвечающая за подключение к системной шине
     input  logic        clk_i,
     input  logic        rst_i,
     input  logic        req_i,
     input  logic        write_enable_i,
     input  logic [31:0] addr_i,
-    input  logic [31:0] write_data_i,  // Не используется, поскольку память
-                                     // реализована в переключателях
+    input  logic [31:0] write_data_i,  // не используется, добавлен для
+                                     // совместимости с системной шиной
     output logic [31:0] read_data_o,
 
-    // Интерфейс контроллера прерываний, сигнализирующий процессору о возникновении необработанного прерывания
+    /*
+        Часть интерфейса модуля, отвечающая за отправку запросов на прерывание
+        процессорного ядра
+    */
 
     output logic        interrupt_request_o,
     input  logic        interrupt_return_i,
 
-    // Интерфейс внешнего порта, предназначенный для взаимодействия с переключателями
+    // Часть интерфейса модуля, отвечающая за подключение к периферии
     input logic [15:0]  sw_i
 );
 
-logic [15:0] registr_sw;
+// Регистр для хранения предыдущего состояния переключателей
+logic [15:0] reg_sw;
 
 always_ff @(posedge clk_i) begin
-    if (rst_i) registr_sw <= 0;
+    if (rst_i) reg_sw <= 0;
     else begin
-        if (registr_sw != sw_i & ~interrupt_return_i) interrupt_request_o <= 1;       
+        // Прерывание при изменении sw и отсутствии return
+        if (reg_sw != sw_i & ~interrupt_return_i) 
+             interrupt_request_o <= 1;       
         else interrupt_request_o <= 0;
-        registr_sw <= sw_i;
+        reg_sw <= sw_i; // Обновление текущего состояния
     end
 end
 
 
 always_ff @(posedge clk_i) begin
     if (rst_i) read_data_o <= 0;
-    else if (addr_i == 0 & ~write_enable_i & req_i) read_data_o <= {16'b0, sw_i};
+    // Чтение sw по адресу 0
+    else if (addr_i == 0 & ~write_enable_i & req_i) 
+        read_data_o <= {16'b0, sw_i};
 end
 
 endmodule
